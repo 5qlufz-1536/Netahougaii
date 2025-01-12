82e86cf902e6b5682c2f68ad36490a0d57e229e8
5z-lql2 <5zabc4989@gmail.com>
2025-01-12 17:35:07 +0900

    feat: ✨ Add main Mod files

---
+++ src/main/java/com/saluf/netahougaii/NetahougaiiMod.java
@@ -0,0 +1,11 @@
+package com.saluf.netahougaii;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetahougaiiMod implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
